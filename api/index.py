0ba6895a508cb358bef5de565be933b9044d5616
Sergio <cava@k3b.de>
2024-10-21 15:12:54 +0200

    First running version

--- api/index.py
+++ api/index.py
@@ -1,19 +1,22 @@
 from flask import Flask
-from vercel import KV
+from vercel_kv_sdk import KV
 
 app = Flask(__name__)
 kv = KV()
 
 
+def parse_bool(strval):
+    return str(strval).lower() == "true"
+
+
 @app.get("/estado")
-async def estado():
-    value = await kv.get("estado")
+def estado():
+    value = parse_bool(kv.get("estado"))
     return {"estado": value}
 
 
 @app.get("/toggle")
-async def toggle():
-    current_value = await kv.get("estado")
-    new_value = "false" if str(current_value).lower() == "true" else "true"
-    await kv.put("estado", new_value)
-    return {"estado": new_value}
+def toggle():
+    value = parse_bool(kv.get("estado"))
+    kv.set("estado", not value)
+    return {"estado": not value}
